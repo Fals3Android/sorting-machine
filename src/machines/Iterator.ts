351bbe27a933e8d2f8fa8704b75cf0813404e300
Fals3Android <davilasturias89@gmail.com>
2021-08-13 17:34:45 -0700

    Implemented Bubble Sort Algo

--- src/machines/Iterator.ts
+++ src/machines/Iterator.ts
@@ -1,29 +1,60 @@
 import { createMachine, assign } from "xstate";
-export const isPointerAtEnd = (context) => context.pointer === context.input.length - 1;
-export const isSorted = (context, event: any) => !event.isSorted;
-export const setInput = assign((context, event: any) => ({ input: event.value }));
-export const setPointer = assign((context: any) => ({ pointer: context.pointer + 1 }));
-export const resetContext = assign((context: any) => ({ input: [], pointer: 0 }));
+export const isLeftAtEnd = (context) => context.left >= context.inputLength - 1;
+export const isSorted = (context) => !context.isSorted;
+export const isGreaterThan = (context: any) => context.values[context.left] > context.values[context.left + 1] && context.left >= context.right;
+export const wasSwapNotCalled = (context: any) => context.swapCalls === 0 && context.left >= context.values.length - 1;
+export const setInput = assign((context, event: any) => ({ inputLength: event.length, values: event.values }));
+export const setLeft = assign((context: any) => ({ left: context.left + 1 }));
+export const resetLeftPointer = assign((context: any) => ({ left: 0, swapCalls: 0 }));
+export const resetContext = assign((context: any) => ({left: 0 }));
+export const swap = assign((context: any) => {
+    let newInput = [...context.values];
+    if(newInput[context.left] > newInput[context.left + 1]) {
+        let temp = newInput[context.left];
+        newInput[context.left] = newInput[context.left + 1];
+        newInput[context.left + 1] = temp;
+        return {
+            swapCalls: context.swapCalls + 1,
+            values: newInput
+        }
+    }
+    return {
+        values: newInput
+    }
+});
+export const setIsSorted = assign((context: any) => ({isSorted: true}));
+
 
 export const Iterator = createMachine(
     {
         id: "iterator",
         initial: "idle",
         context: {
-            input: [],
-            pointer: 0
+            isSorted: false,
+            values: [],
+            inputLength: 0,
+            left: 0,
+            swapCalls: 0
         },
         states: {
             idle: {
                 on: {
-                    START: { target: "active", cond: isSorted, actions: "setInput" }
+                    START: { target: "leftActive", cond: "isSorted", actions: "setInput" }
                 }
             },
-            active: {
+            leftActive: {
                 after: {
                     1000: [
-                        { target: "idle", cond: "isPointerAtEnd", actions: "resetContext" },
-                        { target: "active", actions: "setPointer", }
+                        { target: "idle", cond: "wasSwapNotCalled", actions: "setIsSorted"},
+                        { target: "leftActive", cond: "isLeftAtEnd", actions: "resetLeftPointer" },
+                        { target: "swapActive"},
+                    ]
+                }
+            },
+            swapActive: {
+                after: {
+                    500: [
+                        { target: "leftActive", actions: ["swap", "setLeft"] }
                     ]
                 }
             }
@@ -31,13 +62,18 @@ export const Iterator = createMachine(
     },
     {
         guards: {
-            isPointerAtEnd,
-            isSorted
+            isLeftAtEnd,
+            isSorted,
+            wasSwapNotCalled,
+            isGreaterThan
         },
         actions: {
             setInput,
-            setPointer,
-            resetContext
+            setLeft,
+            resetContext,
+            resetLeftPointer,
+            swap,
+            setIsSorted
         }
     }
 );
--- src/view.tsx
+++ src/view.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useEffect } from "react";
 import { useMachine } from "@xstate/react";
 import { Sorter } from "./machines/Sorter";
 import { Iterator } from "./machines/Iterator";
@@ -6,21 +7,21 @@ import { Iterator } from "./machines/Iterator";
 export function View() {
     const [getSorter, sendToSorter] = useMachine(Sorter);
     const [getIterator, sendToIterator] = useMachine(Iterator);
-    const { pointer } = getIterator.context as any;
-    const { isSorted, input } = getSorter.context as any;
+    const { left, values } = getIterator.context as any;
+    const { input } = getSorter.context as any;
 
     return (
         <div className="numbers-sandbox">
-            <div>Is Sorted: {`${isSorted}`}</div>
+            <p>Bubble Sort</p>
             <input type="text" onChange={(event) => sendToSorter({ type: "START", value: event.target.value })} />
-            {!isSorted ? <button className="sort-button"
-                onClick={() => sendToIterator({ type: "START", value: input, isSorted })}
+            <button className="sort-button"
+                onClick={() => sendToIterator({ type: "START", length: input.length, values: input})}
             >
                 Sort Input
-            </button> : null}
-            <div>{pointer}</div>
+            </button>
+            <div>{left}</div>
             <div className="numbers-container">
-                {input.map((num, v) => <div key={v} className={`box ${pointer == v ? "active" : ""}`} >{num}</div>)}
+                {values.map((num, v) => <div key={v} className={`box ${left + 1 == v ? "right" : ""}  ${left == v ? "left" : ""}`} >{num}</div>)}
             </div>
         </div>
     )
